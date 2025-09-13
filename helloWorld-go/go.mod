efae2d6d10b146547b58287856ca14372c0c51ef
Maycon R Santana <31290067+MayconInforGames2017@users.noreply.github.com>
2025-09-13 11:51:48 -0300

    hello world

---
+++ helloWorld-go/go.mod
@@ -0,0 +1,3 @@
+module github.com/MayconInforGames2017/iniciando-linguagem-go/helloWorld-go
+
+go 1.25.1
---
+++ helloWorld-go/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/MayconInforGames2017/iniciando-linguagem-go/helloWorld-go/meet"
+)
+
+func main()  {
+	meet.SayHello()
+}
